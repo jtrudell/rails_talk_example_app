6bcddc4d77fc5db4994875def0747decb2018a16
Jen Trudell <jtrudell@gmail.com>
2015-11-09 20:59:12 -0600

    add model and migration

---
+++ ponyville/db/migrate/20151110025616_create_ponies.rb
@@ -0,0 +1,14 @@
+class CreatePonies < ActiveRecord::Migration
+  def change
+    create_table :ponies do |t|
+      t.string :name
+      t.string :cutie_mark
+      t.string :type
+      t.string :address
+      t.string :occupation
+      t.string :picture
+
+      t.timestamps null: false
+    end
+  end
+end
